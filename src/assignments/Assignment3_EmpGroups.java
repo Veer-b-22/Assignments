17206b9a9d2d661a1876ee75ab4d293fa6d57bb2
veer <qa@nadsoftdev.com>
2025-03-25 18:01:33 +0530

    Assignment3

--- src/assignments/Assignment3_EmpGroups.java
+++ src/assignments/Assignment3_EmpGroups.java
@@ -4,12 +4,17 @@ public class Assignment3_EmpGroups {
 
 	public static void main(String[] args) {
 		Assignment3_Employees emp = new Assignment3_Employees();
-		String[] EmployeeNames = new String[2];
-		String[] EmployeeIDs = new String[2];
-		EmployeeIDs[0] = "1234";
-		EmployeeNames[0] = "Bharath";
-		System.out.print("Employee Name : " + EmployeeNames[0] + " " + EmployeeIDs[0]);
-		// System.out.print(EmployeeIDs[0]);
+		/*
+		 * String[] EmployeeNames = new String[2]; String[] EmployeeIDs = new String[2];
+		 * EmployeeIDs[0] = "1234"; EmployeeNames[0] = "Bharath";
+		 * System.out.print("Employee Name : " + EmployeeNames[0] + " " +
+		 * EmployeeIDs[0]); // System.out.print(EmployeeIDs[0]);
+		 */
+
+		String[] employeenames = { emp.employeeName1, emp.employeeName2, emp.employeeName3 };
+		int[] employeeId = { emp.employeeID1, emp.employeeID2, emp.employeeID3 };
+
+		System.out.println(employeenames[0] + ":" + employeeId[0]);
 
 	}
 
--- src/assignments/Assignment3_Employees.java
+++ src/assignments/Assignment3_Employees.java
@@ -1,8 +1,16 @@
 package assignments;
 
 public class Assignment3_Employees {
-	String[] EmployeeNames = { "Bharath", "ABC", "XYZ" };
-	String[] EmployeeIDs = { "1234", "1235", "1236" };
+	//	String[] EmployeeNames = { "Bharath", "ABC", "XYZ" };
+	// String[] EmployeeIDs = { "1234", "1235", "1236" };
+	String employeeName1 = "Bharath";
+	int employeeID1 = 1234;
+
+	String employeeName2 = "ABC";
+	int employeeID2 = 1235;
+
+	String employeeName3 = "XYZ";
+	int employeeID3 = 1236;
 
 	public static void main(String[] args) {
 
